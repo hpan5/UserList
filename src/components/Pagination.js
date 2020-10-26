3e31bca946e83a0f23e7f4c8d49c12d84f5bbecd
Doris <hpan996@gmail.com>
2020-10-26 00:50:13 -0700

    added page buttons

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as actionCreator from '../actions/actions';
-
-const Pagination = ({totalUsers, usersPerPage, paginate}) => {
+import './Pagination.css'
+const Pagination = ({totalUsers, usersPerPage, currentPage, paginate}) => {
     const pageNumbers = [];
     let pageNum = Math.ceil(totalUsers / usersPerPage);
     //console.log(pageNum);
     for (let i = 1; i <= pageNum; i++) {
         pageNumbers.push(i);
     }
+
     return (
         <nav>
-            <ul className='pagination'>
-                {pageNumbers.map(number => (
-                    <li key={number} className='page-item'>
-                        <a onClick={() => paginate(number)} className='page-link'>{number}</a>
-                    </li>
-                ))
-                }
-            </ul>
+            <button onClick={() => paginate(currentPage - 1)} disabled={currentPage == 1}>Prev Page</button>
+            <button  onClick={() => paginate(currentPage + 1)} disabled={currentPage == pageNum}>Next Page</button>
+            <p>Page : {currentPage}</p>
         </nav>
     );
 }
@@ -26,10 +22,19 @@ const Pagination = ({totalUsers, usersPerPage, paginate}) => {
 const mapStateToProps = (state, props) => {
     return {
         usersPerPage: state.list.usersPerPage,
-        totalUsers: state.list.filteredUsersNum
+        totalUsers: state.list.filteredUsersNum,
+        currentPage: state.list.currentPage
     }
 }
-
+/*<ul className='pagination'>
+                {pageNumbers.map(number => (
+                    <li key={number} className='page-item'>
+                        <a onClick={() => paginate(number)} className='page-link'>{number}</a>
+                    </li>
+                ))
+                }
+            </ul> 
+*/
 const mapDispatchToProps = (dispatch) => {
     return {
         paginate: (number) => dispatch(actionCreator.paginate(number))
